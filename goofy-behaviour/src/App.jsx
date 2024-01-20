ee0d3683d332a9fec8b47eadd8d14955b1f18eee
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2024-01-20 18:25:44 -0500

    Update HTML and CSS files

--- goofy-behaviour/src/App.jsx
+++ goofy-behaviour/src/App.jsx
@@ -1,9 +1,14 @@
 import SliderComponent from './components/SliderComponent';
-
+import TimeEstimatorScreen from './components/TimeEstimatorScreen';
+import './App.css'; // Import your CSS file (if it's separate)
+//
 function App() {
   return (
     <div className="App">
       <SliderComponent />
+      <div className='center-style'>
+        <TimeEstimatorScreen />
+      </div>
     </div>
   );
 }
