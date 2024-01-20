001df2575176ad320f3e3188f4137d528e5a394e
Gabriel <gabrieldubois.eng@gmail.com>
2024-01-20 16:40:54 -0500

    gitignore and removing localhost for max

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/add": {"origins": "http://localhost:5173"}})
+CORS(app)
 
 @app.route('/add', methods=['POST'])
 def add_five():
--- goofy-behaviour/src/components/SliderComponent.jsx
+++ goofy-behaviour/src/components/SliderComponent.jsx
@@ -11,7 +11,7 @@ const SliderComponent = () => {
 
     const handleSliderRelease = async () => {
         try {
-            const response = await axios.post('http://localhost:5000/add', { number: parseInt(value) });
+            const response = await axios.post('http://127.0.0.1:5000/add', { number: parseInt(value) });
             setResult(response.data.result);
         } catch (error) {
             console.error('Error fetching data:', error);
