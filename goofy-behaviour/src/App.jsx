76c232a633c4b055e168eedb23e66f5c4ef7bc43
DENIS OH <98143219+Denis-Oh@users.noreply.github.com>
2024-01-20 22:12:28 -0500

    Character Input

--- goofy-behaviour/src/App.jsx
+++ goofy-behaviour/src/App.jsx
@@ -1,15 +1,15 @@
 import SliderComponent from "./components/SliderComponent";
 import PlayButton from "./components/PlayButton";
 import BackgroundVid from "./components/BackgroundVid";
-import myImage from "./assets/survivor1.png"; // Import the image
 import "./App.css";
+import CharacterInput from "./components/CharacterInput";
 
 function App() {
   return (
     <div className="App">
       <div className="App-body center-style">
         <BackgroundVid />
-        <img src={myImage} alt="Centered" className="centered-image" />
+        <CharacterInput/>
         <SliderComponent />
         <PlayButton className="bottom-center" />
       </div>
