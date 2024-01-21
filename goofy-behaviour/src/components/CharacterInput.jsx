857fc067e8e8afa349783811729e61641caee9c1
DENIS OH <98143219+Denis-Oh@users.noreply.github.com>
2024-01-20 23:22:52 -0500

    Character inputs added

--- goofy-behaviour/src/components/CharacterInput.jsx
+++ goofy-behaviour/src/components/CharacterInput.jsx
@@ -5,7 +5,7 @@ const CharacterInput = () => {
     <div className="charInput">
       <div className="inputCol">
         <label htmlFor="location">TIME:</label>
-        <input type="time" id="time" name="time" />
+        <input type="time" id="time" name="time" className="otherInput" />
         <label htmlFor="day">DAY:</label>
         <select id="day" name="day">
           <option>Mon</option>
@@ -16,27 +16,40 @@ const CharacterInput = () => {
           <option>Sat</option>
           <option>Sun</option>
         </select>
-        <label htmlFor="partySize">PARTY SIZE:</label>
-        <select id="partySize" name="partySize">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-        </select>
+        <label htmlFor="rankName">RANK:</label>
+        <div className="rankSelect">
+            <select id="rankName" name="rankName">
+                <option>Ash</option>
+                <option>Bronze</option>
+                <option>Silver</option>
+                <option>Gold</option>
+                <option>Iridescent</option>
+            </select>
+            <select id="rankNum" name="rankNum">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            </select>
+        </div>
       </div>
       <img src={myImage} alt="Centered" className="centered-image" />
       <div className="inputCol">
-        <label htmlFor="location">TIME:</label>
-        <input type="time" id="time" name="time" />
-        <label htmlFor="day">DAY:</label>
-        <select id="day" name="day">
-          <option>Mon</option>
-          <option>Tue</option>
-          <option>Wed</option>
-          <option>Thu</option>
-          <option>Fri</option>
-          <option>Sat</option>
-          <option>Sun</option>
+        <label htmlFor="modelType">MODEL TYPE:</label>
+        <div className="otherInput">
+            <label>
+                <input type="radio" name="modelType" value="1" className="modelTypeSelect" />
+                1
+                <input type="radio" name="modelType" value="2" className="modelTypeSelect" />
+                2
+                <input type="radio" name="modelType" value="3" className="modelTypeSelect" />
+                3
+            </label>
+        </div>
+        <label htmlFor="server">SERVER:</label>
+        <select id="server" name="server">
+          <option>us-west-2</option>
         </select>
         <label htmlFor="partySize">PARTY SIZE:</label>
         <select id="partySize" name="partySize">
