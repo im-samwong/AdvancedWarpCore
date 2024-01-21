33fa895553d339135bad19f8003415bc28b13b53
Gabriel <gabrieldubois.eng@gmail.com>
2024-01-20 23:49:49 -0500

    minor ui adjustments

--- goofy-behaviour/src/components/CharacterInput.jsx
+++ goofy-behaviour/src/components/CharacterInput.jsx
@@ -30,7 +30,6 @@ const CharacterInput = () => {
             <option>2</option>
             <option>3</option>
             <option>4</option>
-            <option>5</option>
             </select>
         </div>
       </div>
