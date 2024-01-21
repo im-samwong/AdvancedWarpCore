9e862dcda125f8015c21083677bf79fb973adaf0
DENIS OH <98143219+Denis-Oh@users.noreply.github.com>
2024-01-20 19:03:57 -0500

    Button Component

--- goofy-behaviour/src/App.jsx
+++ goofy-behaviour/src/App.jsx
@@ -1,9 +1,12 @@
 import SliderComponent from './components/SliderComponent';
+import TestComponent from './components/TestComponent';
+
 
 function App() {
   return (
     <div className="App">
       <SliderComponent />
+      <TestComponent />
     </div>
   );
 }
