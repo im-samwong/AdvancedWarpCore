c77877d9dbf4012b4bfff983035a7c964df87251
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2024-01-20 23:59:57 -0500

    Update CSS and input handling in CharacterInput component

--- goofy-behaviour/src/components/CharacterInput.jsx
+++ goofy-behaviour/src/components/CharacterInput.jsx
@@ -1,13 +1,63 @@
-import myImage from "../assets/survivor1.png"; // Import the image
+import useStore from "../store"; // Update the path as needed
+import survivorImage from "../assets/survivor1.png";
+import killerImage from "../assets/killer1.png";
+import "./CharacterInput.css";
 
 const CharacterInput = () => {
+  const {
+    setTime,
+    setDay,
+    setRankName,
+    setRankNum,
+    setModelType,
+    setServer,
+    setPartySize,
+  } = useStore();
+
+  const { isSurvivor } = useStore();
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case "time":
+        setTime(value);
+        break;
+      case "day":
+        setDay(value);
+        break;
+      case "rankName":
+        setRankName(value);
+        break;
+      case "rankNum":
+        setRankNum(value);
+        break;
+      case "modelType":
+        setModelType(value);
+        break;
+      case "server":
+        setServer(value);
+        break;
+      case "partySize":
+        setPartySize(value);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="charInput">
       <div className="inputCol">
-        <label htmlFor="location">TIME:</label>
-        <input type="time" id="time" name="time" className="otherInput" />
+        <label htmlFor="time">TIME:</label>
+        <input
+          type="time"
+          id="time"
+          name="time"
+          className="otherInput"
+          onChange={handleInputChange}
+        />
         <label htmlFor="day">DAY:</label>
-        <select id="day" name="day">
+        <select id="day" name="day" onChange={handleInputChange}>
           <option>Mon</option>
           <option>Tue</option>
           <option>Wed</option>
@@ -18,41 +68,47 @@ const CharacterInput = () => {
         </select>
         <label htmlFor="rankName">RANK:</label>
         <div className="rankSelect">
-            <select id="rankName" name="rankName">
-                <option>Ash</option>
-                <option>Bronze</option>
-                <option>Silver</option>
-                <option>Gold</option>
-                <option>Iridescent</option>
-            </select>
-            <select id="rankNum" name="rankNum">
+          <select id="rankName" name="rankName" onChange={handleInputChange}>
+            <option>Ash</option>
+            <option>Bronze</option>
+            <option>Silver</option>
+            <option>Gold</option>
+            <option>Iridescent</option>
+          </select>
+          <select id="rankNum" name="rankNum" onChange={handleInputChange}>
             <option>1</option>
             <option>2</option>
             <option>3</option>
             <option>4</option>
             <option>5</option>
-            </select>
+          </select>
         </div>
       </div>
-      <img src={myImage} alt="Centered" className="centered-image" />
+      <div className="imageContainer">
+        <img
+          src={isSurvivor ? survivorImage : killerImage}
+          alt="Character"
+          className="centered-image"
+        />
+      </div>
       <div className="inputCol">
-        <label htmlFor="modelType">MODEL TYPE:</label>
-        <div className="otherInput">
-            <label>
-                <input type="radio" name="modelType" value="1" className="modelTypeSelect" />
-                1
-                <input type="radio" name="modelType" value="2" className="modelTypeSelect" />
-                2
-                <input type="radio" name="modelType" value="3" className="modelTypeSelect" />
-                3
-            </label>
+        <div className="modelTypeSlider">
+          <label htmlFor="modelType">MODEL TYPE:</label>
+          <input
+            type="range"
+            min="1"
+            max="3"
+            step="1"
+            name="modelType"
+            onChange={handleInputChange}
+          />
         </div>
         <label htmlFor="server">SERVER:</label>
-        <select id="server" name="server">
+        <select id="server" name="server" onChange={handleInputChange}>
           <option>us-west-2</option>
         </select>
         <label htmlFor="partySize">PARTY SIZE:</label>
-        <select id="partySize" name="partySize">
+        <select id="partySize" name="partySize" onChange={handleInputChange}>
           <option>1</option>
           <option>2</option>
           <option>3</option>
