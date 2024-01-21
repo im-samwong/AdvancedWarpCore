e8c19036d5c9c76389301a6b648799b20a9ea645
Artur Gubaidullin <artur.gubaidullin@gmail.com>
2024-01-20 22:09:01 -0500

    Removed thingie

--- goofy-behaviour/src/App.jsx
+++ goofy-behaviour/src/App.jsx
@@ -1,4 +1,4 @@
-import SliderComponent from "./components/SliderComponent";
+// import SliderComponent from "./components/SliderComponent";
 import PlayButton from "./components/PlayButton";
 import BackgroundVid from "./components/BackgroundVid";
 import myImage from "./assets/survivor1.png"; // Import the image
@@ -10,7 +10,7 @@ function App() {
       <div className="App-body center-style">
         <BackgroundVid />
         <img src={myImage} alt="Centered" className="centered-image" />
-        <SliderComponent />
+        {/* <SliderComponent /> */}
         <PlayButton className="bottom-center" />
       </div>
     </div>
